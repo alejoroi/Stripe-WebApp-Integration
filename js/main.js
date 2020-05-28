a5dc85d76b9be6a6fe3e20a2049bdb54df8d29f3
Alejandro Velandia <alexandrevelandia@gmail.com>
2020-05-28 14:00:55 -0400

    Client-Server Project with Stripe

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-window.onload = () => { 
-
- /* ---------------------------------------------
-                    PARALLAX EFECT
---------------------------------------------- */ 
-const parallax = document.getElementById("parallax");
-
-window.addEventListener("scroll", function()
-{
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.4 + "px";
-    // console.log(offset);
-});
 
 /* ---------------------------------------------
                     NAVBAR CONTROL
